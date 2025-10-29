18fe4651910d032c9115f40459bce2e6922aab12
sheenaas13 <sheenaas013@gmail.com>
2025-10-29 16:51:56 +0530

    forgot pass option working

--- Mainpage/url.py
+++ Mainpage/url.py
@@ -44,4 +44,23 @@ path('product_listing', views.product_listing, name='product_listing'),
 
 path('search/', views.search_products, name='search_products'),
 
+path('password_reset/', 
+         auth_views.PasswordResetView.as_view(template_name='password_reset.html'), 
+         name='password_reset'),
+
+    # Step 2: After sending email
+path('password_reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), 
+         name='password_reset_done'),
+
+    # Step 3: User sets new password
+path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+     name='password_reset_confirm'),
+
+    # Step 4: Password successfully changed
+path('reset/done/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), 
+         name='password_reset_complete'),
+
 ]
